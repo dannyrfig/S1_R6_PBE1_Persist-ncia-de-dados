c01d33d7f70ce1939331b83e73776efb1776df0b
unknown <danielly.figueiredo@aluno.senai>
2025-10-07 15:40:17 -0300

    S1_R6-AT1_PBE1

--- src/models/produtoModel.js
+++ src/models/produtoModel.js
@@ -14,8 +14,26 @@ const produtoModel = {
 
 
         } catch (error) {
-            console.log('Erro: ao buscar produtos:',error);xzz
+            console.log('Erro: ao buscar produtos:',error);
             throw error; //
+        }
+    },
+
+    inserirProduto: async (nomeProduto,precoProduto)=>{
+        try {
+            
+            const pool = await getConnection();
+            
+            let querySQL = 'INSERT INTO Produtos(nomeProduto, PrecoProduto) VALUES(@nomeProduto, @precoProduto)';
+
+            await pool.request()
+            .input('nomeProduto',sql.VarChar(100), nomeProduto)
+            .input('precoProduto',sql.Decimal(10,2), precoProduto)
+            .query(querySQL);
+            
+        } catch (error) {
+            console.error('Erro ao inserir produto:',error);
+            throw error; //Passa o erro para o controller tratar
         }
     }
 }
